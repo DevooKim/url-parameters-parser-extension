8a7fa609e34a07a960e90e850326b2fda48ec8bf
DevooKim <khwoo8026@gmail.com>
2025-03-17 00:59:05 +0900

    UI 구성 요소 업데이트: Tooltip 컴포넌트 제거 및 Select 컴포넌트 추가

--- packages/ui/lib/components/ui/index.ts
+++ packages/ui/lib/components/ui/index.ts
@@ -1,4 +1,4 @@
 export * from './button';
 export * from './card';
 export * from './separator';
-export * from './tooltip';
+export * from './select';
--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -1,6 +1,17 @@
 import { parseUrl, useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
-import { Card, CardContent, CardHeader, CopyButton, Separator, useButtonClassName } from '@extension/ui';
+import {
+  Card,
+  CardContent,
+  CopyButton,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Separator,
+  useButtonClassName,
+} from '@extension/ui';
 import { useState } from 'react';
 import { ParsedItem } from './ParsedItem';
 
@@ -13,6 +24,11 @@ const testOptions = [
     hostname: 'www.example.com',
     patterns: ['/aaa/:a-id', '/bbb/:b-id', '/ccc/:c-id'],
   },
+  {
+    name: 'example2',
+    hostname: 'www.example.com',
+    patterns: ['/aaa/:a-id'],
+  },
 ];
 
 const Popup = () => {
@@ -21,7 +37,9 @@ const Popup = () => {
   const [optionName, setOptionName] = useState('example');
   const { className: buttonClassName, handleSuccess, handleFail } = useButtonClassName();
 
-  const option = testOptions.find(option => option.name === optionName);
+  const options = testOptions;
+
+  const option = options.find(option => option.name === optionName);
 
   // const url = useUrl();
   const url = testUrl;
@@ -30,15 +48,33 @@ const Popup = () => {
   const parsedUrl = parseUrl(url, patterns);
 
   return (
-    <Card className="w-[420px] p-5 shadow-none border-0 flex flex-col gap-3">
-      <CardHeader className="flex items-center justify-between gap-1 p-0 space-y-0">
+    <Card className="w-[420px] p-5 shadow-none border-0 flex flex-col gap-4">
+      <CardContent className="flex flex-col items-center justify-between gap-1 p-0">
         <div className="w-full">
           <p className="text-sm font-medium break-all">{url}</p>
         </div>
         <CopyButton value={url} onSuccessCopy={handleSuccess} onFailCopy={handleFail} className={buttonClassName}>
           복사하기
         </CopyButton>
-      </CardHeader>
+      </CardContent>
+      <Separator />
+
+      <CardContent className="flex flex-col items-center justify-between gap-3 p-0">
+        <Select value={optionName} onValueChange={setOptionName}>
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent position="popper">
+            {options.map(option => (
+              <SelectItem key={option.name} value={option.name}>
+                {option.name}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+
+        <div>{JSON.stringify(patterns)}</div>
+      </CardContent>
       <Separator />
       <CardContent className="p-0">
         <div className="flex flex-col items-center justify-between gap-1">
