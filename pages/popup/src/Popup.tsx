36669bde4e71a9d2db3f4e21bb044f475e2e61ac
DevooKim <khwoo8026@gmail.com>
2025-03-17 03:46:44 +0900

    UI 구성 요소 업데이트: Popup 컴포넌트의 코드 스타일 수정으로 긴 문자열이 줄 바꿈되도록 변경

--- pages/popup/src/Popup.tsx
+++ pages/popup/src/Popup.tsx
@@ -61,7 +61,7 @@ const Popup = () => {
           </SelectContent>
         </Select>
 
-        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-semibold">
+        <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-xs font-semibold break-all">
           {JSON.stringify(patterns)}
         </code>
       </CardContent>
